de47e9bfd9af22afde4dca83e8414cd176223554
avilesdiana <avilesdiana@outlook.com>
2018-04-26 12:52:34 -0500

    Cambios main

--- Dijkstra.c
+++ Dijkstra.c
@@ -1,1 +1,11 @@
-//main del proyecto
+/*----------------------------------------------------------------------------------------------------
+P R O Y E C T O    F I N A L     <TALLER DESARROLLO DE APLICACIONES
+---------------------------------------------------------------------------------------------------*/
+#include <stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc, char const *argv[]) {
+  printf("Menú\n");
+  return 0;
+}
--- makefile
+++ makefile
@@ -1,10 +1,9 @@
-default: topologia.o funciones.o
-	gcc topologia.o funcionesTopologia.o -o Dijkstra -lm
-topologia.o:topologia.c
-	gcc -c topologia.c
-funciones.o:funcionesTopologia.c
-	gcc -c funcionesTopologia.c
+default: Dijkstra.o
+	gcc Dijkstra.o -o Dijkstra -lm
+Dijkstra.o:Dijkstra.c
+	gcc -c Dijkstra.c
+
 todo:
-	gcc topologia.c funcionesTopologia.c -o Dijkstra
+	gcc Dijkstra.c -o Dijkstra
 clear:
 	rm*.o
