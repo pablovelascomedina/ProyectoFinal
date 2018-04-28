c32ad5b37c372ece475b2af1f963b1cfaf2eda2d
avilesdiana <avilesdiana@outlook.com>
2018-04-28 16:27:28 -0500

    menu

--- Dijkstra.c
+++ Dijkstra.c
@@ -2,8 +2,26 @@
 P R O Y E C T O    F I N A L     <TALLER DESARROLLO DE APLICACIONES
 ---------------------------------------------------------------------------------------------------*/
 
-
 int main(int argc, char const *argv[]) {
-  printf("Menú\n");
+
+  system ("clear");
+
+    
+    printf("\n\n\t\t\t\t\tPROGRAMA  DE  CAMINOS  CORTOS \n\n");
+    printf("\t\t\t\t     M e n u      d e      O p c  i o n e s \n\n");
+    printf("\t\t\t\t  --------------------------------------------\n");
+    printf("\t\t\t\t\t\tDesarrollado por:\n\n");
+    printf(" \t\t\t\t\t Diana Laura Aviles Elizalde\n" );
+    printf(" \t\t\t\t\t Diego Patricio Gutierrez Lopez Cano\n" );
+    printf(" \t\t\t\t\t Juan Pablo Velasco Medina\n" );
+    printf("\t\t\t\t  --------------------------------------------\n");
+    printf("\t\t |                                                                             |\n");
+    printf("\t\t |  1.  Ayuda                                                                  |\n");
+    printf("\t\t |  2.  Generar topología                                                      |\n");
+    printf("\t\t |  3.  Crear las tablas de ruteo                                              |\n");
+    printf("\t\t |  4.  Mostrar la lista de nodos de la red                                    |\n");
+    printf("\t\t |                                                                             |\n\n");
+    printf("\t\t\t\tIngresar opcion: ");
+
   return 0;
 }
